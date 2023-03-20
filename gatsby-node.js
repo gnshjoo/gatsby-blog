be30bb7eb9a5b3f8b156adfaa70f207b25dc5a76
gnshjoo <gnshjoo@gmail.com>
2023-03-20 16:39:37 +0900

    init

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,3 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
- */
-
-/**
- * @type {import('gatsby').GatsbyNode['createPages']}
- */
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
@@ -14,5 +5,23 @@ exports.createPages = async ({ actions }) => {
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+}
+
+const path = require("path")
+
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, "src/components"),
+        utils: path.resolve(__dirname, "src/utils"),
+        hooks: path.resolve(__dirname, "src/hooks"),
+      },
+    },
   })
 }
---
+++ src/pages/info.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Text from 'components/Text'
+import { graphql } from 'gatsby'
+
+type InfoPageProps = {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+        description: string
+        author: string
+      }
+    }
+  }
+}
+
+const InfoPage = ({
+  data: {
+    site: {
+      siteMetadata: { title, description, author },
+    },
+  },
+}: InfoPageProps) => {
+  return (
+    <div>
+      <Text text={title} />
+      <Text text={description} />
+      <Text text={author} />
+    </div>
+  )
+}
+
+export default InfoPage
+
+export const metadataQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`
