92097b3e6c9815020728f177dc6e2d32ed8c1ed3
gnshjoo <gnshjoo@gmail.com>
2023-03-20 17:37:30 +0900

    init profileImg add

---
+++ src/components/Main/Profile.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled'
+import { FunctionComponent } from 'react'
+
+const Profile_IMG =
+  'https://avatars.githubusercontent.com/u/23391657?s=400&u=8413451c5d94079f05b9b4e6420f496b474e3ac3&v=4'
+
+const ProfileWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+`
+
+const ProfileImage: FunctionComponent = () => {
+  return <ProfileWrapper src={Profile_IMG} alt="Profile IMG" />
+}
+
+export default ProfileImage
